93d217404a4d8a55131e31241a242df2a4385e89
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-01-26 13:29:39 +0530

    Time: 2 ms (43.91%) | Memory: 7.9 MB (8.36%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
+        double ans = 1;
+        if(n<0)
+        {
+            x = 1/x;
         }
-        
-        return pow;
+        n = abs(n);
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans *= x;
+            }
+            n = n / 2;
+            x = x * x;
+        }
+        return ans;
     }
 };
