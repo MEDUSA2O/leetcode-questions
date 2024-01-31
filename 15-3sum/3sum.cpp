45fa80512db7ae06a6dd8b3a30353dcbdd86024b
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-01-31 21:16:11 +0530

    Time: 54 ms (98.08%) | Memory: 26.9 MB (49.64%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,64 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // int n = nums.size();
-        // sort(nums.begin() , nums.end());
-        // vector<int>ans;
-        
-        // for(int i = 0 ; i < n ; i++){
-        //     int a = nums[i];
-        //     int l = i+1;
-        //     int r = n-1;
-        //     if( a +nums[l]+nums[r] == 0 ) {
-        //         ans = {a,l,r};
-        //         return ans;
-        //     }
-        //     else if(  a + l + r > 0) {
-        //         r--;
-        //     }
-        //     else{
-        //         l++;
-        //     }
-        // }
-        // return {0,0,0};
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-
         for (int i = 0; i < n - 2; i++) {
+             if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;  // Skip duplicate values for 'a'
+            }
             int a = nums[i];
             int l = i + 1;
             int r = n - 1;
 
             while (l < r) {
                 int sum = a + nums[l] + nums[r];
-
                 if (sum == 0) {
                     ans.push_back({a, nums[l], nums[r]});
                     l++;
                     r--;
-
-                    // Avoid duplicate triplets
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
                     }
                     while (l < r && nums[r] == nums[r + 1]) {
                         r--;
                     }
-                } else if (sum > 0) {
-                    r--;
-                } else {
+                } else if (sum < 0) {
                     l++;
+                } else {
+                    r--;
                 }
             }
-
-            // Avoid duplicate values for 'a'
-            while (i < n - 2 && nums[i] == nums[i + 1]) {
-                i++;
-            }
+            // while (i < n - 1 && nums[i] == nums[i - 1]) {
+            //     i++;
+            // }
         }
-
         return ans;
-        
     }
 };
