a4e93e60792f14d244c07e70c33eed048ae3beac
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-06 14:04:18 +0530

    Time: 30 ms (60.89%) | Memory: 24.4 MB (25.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagrams; // Mapping sorted string to list of anagrams
-        vector<vector<string>> result;
-
-        // Iterate through each string in the input vector
-        for (const string& s : strs) {
-            // Create a copy of the string and sort it
-            string sortedStr = s;
-            sort(sortedStr.begin(), sortedStr.end());
-
-            // Add the sorted string to the map and append the original string to its list
-            anagrams[sortedStr].push_back(s);
+        unordered_map<string , vector<string>>mp;
+        for( auto a : strs){
+            string temp = a;
+            sort(temp.begin() , temp.end());
+            mp[temp].push_back(a);
         }
+        vector<vector<string>>ans;
+        for( auto a : mp){
+            ans.push_back(a.second);
 
-        // Copy the values from the map to the result vector
-        for (auto& entry : anagrams) {
-            result.push_back(entry.second);
         }
-
-        return result;
+        return ans;
         
     }
 };
