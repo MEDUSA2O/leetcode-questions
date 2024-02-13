dd0d9894f808649fc1d248264ed794e19a8eb323
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-13 14:34:05 +0530

    Time: 61 ms (13.54%) | Memory: 30 MB (7.01%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(string s : words){
+            string reversed = s ;
+            reverse(reversed.begin() , reversed.end()) ;
+            if(reversed == s){
+                return s ;
+            }
+
+        }
+        return "";
+        
+
+        
+    }
+};
