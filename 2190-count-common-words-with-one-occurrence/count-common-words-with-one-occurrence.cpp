7b7ca1b22d36250c08b38066d559316d48fe79c8
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-14 12:01:26 +0530

    Time: 34 ms (40.52%) | Memory: 22.2 MB (29.43%) - LeetSync

---
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string, int> mp;
+        for (auto it : words1) {
+            if (mp[it] == 0)
+                mp[it] = 1;
+            else if (mp[it] == 1)
+                mp[it] = -1;
+        }
+        for (auto it : words2) {
+            if (mp[it] == 1)
+                mp[it] = 2;
+            else if
+                (mp[it] == 2) mp[it] = -1;
+        }
+        int count = 0 ;
+        for(auto o : mp){
+            if(o.second == 2){
+                count++;
+            }
+
+        }
+        return count;
+    }
+};
