847e9fd824159b2300d55e54e61ef29d861f6cad
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-20 19:37:34 +0530

    Time: 39 ms (31.96%) | Memory: 14.6 MB (15.23%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -3,7 +3,7 @@ public:
     bool winnerOfGame(string colors) {
         int alice = 0 ; 
         int bob = 0 ; 
-        for(int i = 1 ; i < colors.size() ; i++){
+        for(int i = 1 ; i < colors.size() -1 ; i++){
             if(colors[i] == colors[i-1] && colors[i] == colors[i+1]){
                 if(colors[i] == 'A') alice++;
                 if(colors[i] == 'B') bob++;
