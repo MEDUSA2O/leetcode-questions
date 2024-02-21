90130974faced68dcd5fb9f607941d190026e478
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-21 22:48:31 +0530

    Time: 82 ms (17.02%) | Memory: 61.6 MB (14.89%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cpp
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
         long long int sum = 0;
-        vector<int> temp;
+        long long int max = INT_MIN;
         for (int j = 0; j < damage.size(); j++) {
             sum += damage[j];
-            temp.push_back(damage[j]);
+            if (damage[j] > max) {
+                max = damage[j];
+            }
         }
         if (armor == 0)
             return sum + 1;
-        sort(temp.begin(), temp.end());
-        if (temp[damage.size() - 1] <= armor) {
-            return sum - temp[damage.size() - 1] + 1;
+        if (max <= armor) {
+            return sum - max + 1;
         }
         return sum - armor + 1;
     }
