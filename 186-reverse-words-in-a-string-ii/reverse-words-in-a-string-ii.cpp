16d198fbcab25c14f9d8b017b79b8055d6a7907b
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-02-23 19:11:42 +0530

    Time: 7 ms (99.80%) | Memory: 19.6 MB (42.80%) - LeetSync

--- 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.cpp
+++ 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     void reverseWords(vector<char>& s) {
-        // Reverse the entire string
         reverse(s.begin(), s.end());
-
         int start = 0;
         int end = 0;
         int n = s.size();
-
         while (start < n) {
-            // Find the end of the current word
             while (end < n && s[end] != ' ') {
                 end++;
             }
-
-            // Reverse the current word [start, end)
             reverse(s.begin() + start, s.begin() + end);
-
-            // Move to the next word
             start = end + 1;
             end = start;
         }
