77a099f4d6b2f525ec727036fea7df59805d3b46
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-03-01 11:40:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (40.79%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -7,22 +7,20 @@ using namespace std;
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        vector<char> ans;
-        int n = s.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') {
-                count++;
-            }
+        string ans ;      
+        int sz = s.size() ;
+
+        int ones = count ( s.begin() , s.end() , '1');
+         ans += '1' ;
+         ones -- ;
+        int zeros = sz - ones;
+        while( zeros-- > 1){
+            ans += '0';
         }
-        for (int j = count; j > 1; j--) {
-            ans.push_back('1');
+        while( ones-- > 0){
+            ans += '1';
         }
-        for (int j = n - count; j > 0; j--) {
-            ans.push_back('0');
-        }
-        ans.push_back('1');
-        string str(ans.begin(), ans.end());
-        return str;
+        reverse( ans.begin() , ans.end() );
+        return ans;
     }
 };
