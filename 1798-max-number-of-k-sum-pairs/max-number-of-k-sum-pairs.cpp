b466ae8d8ebe106561995fa65ecc90a609a7c401
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-03-02 22:54:43 +0530

    Time: 164 ms (10.96%) | Memory: 76.4 MB (10.24%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        int count = 0; 
-        sort(nums.begin(), nums.end()); // Sort the vector to use binary search
-
-        for(int i = 0; i < nums.size(); ++i) {
-            auto it = lower_bound(nums.begin() + i + 1, nums.end(), k - nums[i]);
-            if (it != nums.end() && *it == k - nums[i]) {
-                nums.erase(it);
-                nums.erase(nums.begin() + i);
+        unordered_map<int, int> map;
+        int count = 0;
+        // build the hashmap with count of occurence of every element in array
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]] = map[nums[i]] + 1;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int current = nums[i];
+            int complement = k - nums[i];
+            if (map[current] > 0 && map[complement] > 0) {
+                if ((current == complement) && map[current] < 2) continue;
+                map[current] = map[current] - 1;
+                map[complement] = map[complement] - 1;
                 count++;
-                i--; // Adjust index after erasing an element
             }
         }
         return count;
