c58ee59244a5a29fe1a7423254a054150faa7bcf
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-03-08 12:46:37 +0530

    Time: 266 ms (77.78%) | Memory: 198.3 MB (100.00%) - LeetSync

---
+++ 3359-linked-list-frequency/linked-list-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* frequenciesOfElements(ListNode* head) {
+        unordered_map<int, int> mp;
+        ListNode* temp = head;
+        // Correctly traverse the entire linked list
+        while (temp != nullptr) {
+            mp[temp->val]++;
+            temp = temp->next;
+        }
+
+        // Dummy node to ease list construction
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+
+        // Iterate through the map to create new nodes for each frequency
+        for (auto& [num, freq] : mp) {
+            current->next = new ListNode(freq);
+            current = current->next;
+        }
+
+        // The actual head of the frequency list is next to dummy
+        ListNode* ans = dummy->next;
+        delete dummy; // Clean up dummy node
+        return ans;
+    }
+};
