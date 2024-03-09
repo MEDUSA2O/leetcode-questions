eda4335d7543b03f8295ecf185ed35266740b568
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-03-09 12:08:53 +0530

    Time: 78 ms (35.68%) | Memory: 52.8 MB (84.47%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> mp(nums1.begin(), nums1.end());
-        for (auto it : nums2) {
-            if (mp.contains(it)) {
-                return it;
+        int n = nums1.size() ;
+        int m = nums2.size() ;
+        int j = 0 , i = 0 ;
+        while( i < n && j < m ){
+            if( nums1[i] > nums2[j] )j++;
+            else if ( nums1[i] < nums2[j] )i++;
+            else{
+                return nums1[i];
             }
         }
         return -1;
+        
     }
 };
