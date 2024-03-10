cd53578399ad30278488d56e3ce16123888e8955
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-03-10 11:16:11 +0530

    Time: 12 ms (13.84%) | Memory: 14.1 MB (7.77%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
         unordered_map<int, int> mp;
-        int i = 0;
-        int j = 0;
         vector<int> ans;
         for (auto it : nums1) {
             mp[it]++;
