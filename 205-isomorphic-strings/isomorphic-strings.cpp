c28f3847a1e3ede38fe60d0a4f24bad952bad3be
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-04-02 20:08:08 +0530

    Time: 4 ms (78.16%) | Memory: 9.2 MB (63.23%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,53 +3,20 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> mp;
         unordered_map<char, char> cp;
-        // unordered_map<char,int>mp;
-        // unordered_map<char,int>cp;
-        // for(int i = 0 ; i < s.size() ; i++ ){
-        //     mp[s[i]]++;
-        // }
-        // int count1 = mp.size();
-        // for(int i = 0 ; i < t.size() ; i++ ){
-        //     cp[t[i]]++;
-        // }
-        // int count2 = cp.size();
-        // if( count1 != count2 ) return false;
-        // return true;
-        // for (int i = 0; i < s.size(); i++) {
-        //     // If s[i] is empty (hasn't been assigned yet), store t[i] in the
-        //     // map for it
-        //     if (mp[s[i]] == '\0') {
-        //         mp[s[i]] = t[i];
-        //         if(mp.find(t[i]) != mp.end()){
-
-        //         }
-        //     } else {
-        //         // If mp[s[i]] is not empty and mp[s[i]] != t[i], return false
-        //         if (mp[s[i]] != t[i]) {
-        //             return false;
-        //         }
-        //     }
-        // }
         for (int i = 0; i < s.size(); i++) {
-    // If s[i] is empty (hasn't been assigned yet), store t[i] in the map for it
-    if (mp[s[i]] == '\0') {
-        // Check if t[i] is already present in the map for any other key
-        for (const auto& kv : mp) {
-            if (kv.second == t[i]) {
-                return false;  // t[i] already assigned to another key
+            if (mp[s[i]] == '\0') {
+                for (const auto& kv : mp) {
+                    if (kv.second == t[i]) {
+                        return false;
+                    }
+                }
+                mp[s[i]] = t[i];
+            } else {
+                if (mp[s[i]] != t[i]) {
+                    return false;
+                }
             }
         }
-        // Assign t[i] to mp[s[i]]
-        mp[s[i]] = t[i];
-    } else {
-        // If mp[s[i]] is not empty and mp[s[i]] != t[i], return false
-        if (mp[s[i]] != t[i]) {
-            return false;
-        }
-    }
-}
-
-
         return true;
     }
 };
