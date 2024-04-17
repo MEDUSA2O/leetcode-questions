c30144f3e3463b56f4e0ab0f29a6204c7141a039
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-04-17 22:30:22 +0530

    Time: 108 ms (90.01%) | Memory: 76 MB (86.01%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,18 +1,15 @@
-#include <unordered_set>
-#include <vector>
-
 class Solution {
 public:
-    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-        std::unordered_set<int> set;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (set.find(nums[i]) != set.end()) {
-                
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            
+            if (mp.find(nums[i]) != mp.end()) {
                 return true;
             }
-            set.insert(nums[i]);
+            mp.insert(nums[i]);
             if (i >= k) {
-                set.erase(nums[i - k]);
+                mp.erase(nums[i - k]);
             }
         }
         return false;
