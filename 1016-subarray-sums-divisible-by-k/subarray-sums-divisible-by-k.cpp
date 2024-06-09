3aa4e488baf5de44def9775ea1b32b27bc330d19
MEDUSA2O <123243857+MEDUSA2O@users.noreply.github.com>
2024-06-09 23:31:20 +0530

    Time: 51 ms (22.25%) | Memory: 35.1 MB (44.61%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,21 +1,19 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    int subarraysDivByK(std::vector<int>& nums, int k) {
-        int prefix_sum = 0;
-        int res = 0;
-        std::unordered_map<int, int> prefix_cnt;
-        prefix_cnt[0] = 1;
-
-        for (int n : nums) {
-            prefix_sum += n;
-            int remain = ((prefix_sum % k) + k) % k;  // Ensure the remainder is positive
-            res += prefix_cnt[remain];
-            prefix_cnt[remain]++;
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int sum = 0;
+        int result = 0;
+        mp[0] = 1;
+        for (auto i : nums) {
+            sum += i;
+            int remainder = sum % k;
+            if (remainder < 0) {
+                remainder += k; // Adjust negative remainder to be positive
+            }
+            result += mp[remainder];
+            mp[remainder]++;
         }
-
-        return res;
+        return result;
     }
 };
