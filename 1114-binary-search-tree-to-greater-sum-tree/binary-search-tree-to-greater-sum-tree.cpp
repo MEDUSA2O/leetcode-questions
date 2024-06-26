ff8d0c48e3d3fccf1d561dc8ca83b2443e762505
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-06-26 17:23:14 +0530

    Time: 4 ms (35.73%) | Memory: 10 MB (96.10%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -1,64 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
+private:
+    int sum = 0;
+
+    void traverse(TreeNode* root) {
+        if (root) {
+            traverse(root->right);  // Traverse the right subtree
+            sum += root->val;  // Update the sum
+            root->val = sum;  // Update the current node's value
+            traverse(root->left);  // Traverse the left subtree
+        }
+    }
+
 public:
     TreeNode* bstToGst(TreeNode* root) {
-        if (!root)
-            return nullptr;
-
-        std::vector<int> arr1;
-        std::queue<TreeNode*> q;
-
-        q.push(root);
-        while (!q.empty()) {
-            TreeNode* p = q.front();
-            q.pop();
-            arr1.push_back(p->val);
-
-            if (p->left != nullptr)
-                q.push(p->left);
-
-            if (p->right != nullptr)
-                q.push(p->right);
-        }
-
-        std::sort(arr1.begin(), arr1.end());
-
-        std::vector<int> arr2(arr1.size());
-        int ans = std::accumulate(arr1.begin(), arr1.end(), 0);
-        arr2[0] = ans;
-        for (size_t i = 1; i < arr1.size(); ++i) {
-            arr2[i] = arr2[i - 1] - arr1[i - 1];
-        }
-
-        std::unordered_map<int, int> mp;
-        for (size_t i = 0; i < arr1.size(); ++i) {
-            mp[arr1[i]] = arr2[i];
-        }
-
-        q.push(root);
-        while (!q.empty()) {
-            TreeNode* p = q.front();
-            q.pop();
-            p->val = mp[p->val];
-
-            if (p->left != nullptr)
-                q.push(p->left);
-
-            if (p->right != nullptr)
-                q.push(p->right);
-        }
-
+        traverse(root);
         return root;
     }
 };
