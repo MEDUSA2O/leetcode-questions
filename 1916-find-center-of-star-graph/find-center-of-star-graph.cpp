19406d24f59c4ca4f31f402b24a45e039d7515c5
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-06-28 10:46:12 +0530

    Time: 143 ms (45.45%) | Memory: 85.3 MB (32.13%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
+#define DPSolver ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int, int> degree;
-
-        for (vector<int> edge : edges) {
-            degree[edge[0]]++;
-            degree[edge[1]]++;
-        }
-
-        for (pair<int, int> nodes : degree) {
-            int node = nodes.first;
-            int nodeDegree = nodes.second;
-
-            if (nodeDegree == edges.size()) {
-                return node;
+        DPSolver;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < edges.size(); i++) {
+            for (int j = 0; j < 2; j++) {
+                mp[edges[i][j]]++;
             }
         }
 
-        return -1;
+        int maxi = INT_MIN;
+        int center = -1; // To store the node with the maximum count
+        for (auto& a : mp) {
+            if (a.second > maxi) {
+                maxi = a.second;
+                center = a.first;
+            }
+        }
+        
+        return center;
     }
 };
