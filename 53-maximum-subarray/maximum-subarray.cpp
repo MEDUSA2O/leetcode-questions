73c65e094e358608c5fc4e1acdb0051179ab3200
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-01 10:03:54 +0530

    Time: 76 ms (63.56%) | Memory: 70.2 MB (67.11%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if( n== 0)return 0;
+        int curr = nums[0];
+        int maxi = nums[0];
+        for(int i =1 ; i < n ;i++){
+            curr = max(nums[i] , curr+nums[i]);
+            maxi = max(curr,maxi);
+        }
+        return maxi;
+        
+    }
+};
