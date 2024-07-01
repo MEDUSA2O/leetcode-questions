8b21849f8c3a778292cdea2377ec5d5076bbf767
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-01 19:18:39 +0530

    Time: 5 ms (67.23%) | Memory: 13.5 MB (10.85%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-
+    
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -17,29 +17,20 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head)
-            return nullptr;
-
-        // Create a mapping from original nodes to their corresponding copied
-        // nodes
-        unordered_map<Node*, Node*> oldToCopy;
-        Node* cur = head;
-
-        // First pass: Create all new nodes and store in the map
-        while (cur) {
-            oldToCopy[cur] = new Node(cur->val);
-            cur = cur->next;
+        if(!head)return NULL;
+        Node* curr = head;
+        unordered_map<Node*,Node*>mp;
+        while(curr){
+            mp[curr]  = new Node (curr -> val);
+            curr = curr -> next;
         }
-
-        // Second pass: Set next and random pointers
-        cur = head;
-        while (cur) {
-            oldToCopy[cur]->next = oldToCopy[cur->next];
-            oldToCopy[cur]->random = oldToCopy[cur->random];
-            cur = cur->next;
-        }
-
-        // Return the head of the copied list
-        return oldToCopy[head];
+        curr = head;
+        while( curr ){
+            mp[curr] -> next = mp[curr -> next];
+            mp[curr] -> random = mp[curr -> random];
+            curr = curr -> next;
+         } 
+         return mp[head];
+        
     }
 };
