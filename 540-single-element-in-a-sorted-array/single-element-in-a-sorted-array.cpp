01cacb2e84428f029c96a2c6b790f6e56174e3cf
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-02 16:44:34 +0530

    Time: 13 ms (91.77%) | Memory: 24.7 MB (13.99%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size() == 1)return 1;
+        //if(nums.size() == 1)return 1;
         for(int i = 1 ; i < nums.size() ; i++){
             int c = 0;
             if(nums[i] != nums[i+1] && nums[i-1] != nums[i]){
