fd8eb57f94530ac6c0fb509e7c432885be5b2905
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-02 21:58:13 +0530

    Time: 19 ms (85.10%) | Memory: 94.9 MB (39.90%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,31 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int size1 = nums1.size(), size2 = nums2.size();
-        vector<int> combo(size1 + size2);
-        int i = 0, j = 0, k = 0;
-        
-        // Merge the two arrays
-        while (i < size1 && j < size2) {
-            if (nums1[i] < nums2[j]) {
-                combo[k++] = nums1[i++];
-            } else {
-                combo[k++] = nums2[j++];
+        int i = 0 ; 
+        int j = 0 ;
+        int k = 0;
+        int totalSize = nums1.size() + nums2.size();
+        vector<int> com(totalSize); 
+        while( i < nums1.size()  && j < nums2.size()){
+            if(nums1[i] > nums2[j]){
+                com[k++] = nums2[j++];
+            }
+            else{
+                com[k++] = nums1[i++];
             }
         }
-        
-        // Add remaining elements of nums1, if any
-        while (i < size1) {
-            combo[k++] = nums1[i++];
+        while( i < nums1.size()){
+            com[k++] = nums1[i++];
         }
-        
-        // Add remaining elements of nums2, if any
-        while (j < size2) {
-            combo[k++] = nums2[j++];
+        while( j < nums2.size()){
+            com[k++] = nums2[j++];
         }
-        
-        // Calculate median
-        int totalSize = size1 + size2;
-        if (totalSize % 2 == 0) {
-            return (combo[totalSize/2 - 1] + combo[totalSize/2]) / 2.0;
+       if (com.size() % 2 == 0) {
+            return (com[com.size() / 2 - 1] + com[com.size() / 2]) / 2.0;
         } else {
-            return combo[totalSize/2];
+            return com[com.size() / 2];
         }
+
+        
     }
 };
