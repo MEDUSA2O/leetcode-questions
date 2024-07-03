f1a2989c2077fac4264d900622d3365c7e0c939f
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-03 11:23:01 +0530

    Time: 69 ms (56.53%) | Memory: 38.7 MB (80.72%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        if (nums.size() <= 3) return 0;
+        if (nums.size() <= 4) return 0;
         
         // Sort the array
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        // We consider four scenarios:
-        // 1. Remove the 3 largest elements.
-        // 2. Remove the 2 largest elements and the smallest element.
-        // 3. Remove the largest element and the 2 smallest elements.
-        // 4. Remove the 3 smallest elements.
-        // These scenarios will help in finding the minimum difference after removing up to 3 elements.
-
         int option1 = nums[n - 4] - nums[0];
         int option2 = nums[n - 3] - nums[1];
         int option3 = nums[n - 2] - nums[2];
