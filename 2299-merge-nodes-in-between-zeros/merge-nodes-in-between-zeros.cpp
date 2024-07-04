5fd0477a624ebaba6b75aca0d2bdef9ceb2fdc4f
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-04 12:03:20 +0530

    Time: 466 ms (29.15%) | Memory: 285 MB (6.05%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* x = head->next;  
+        int sum = 0;
+        vector<int> ans;
+
+        while (x != nullptr) {
+            if (x->val != 0) {
+                sum += x->val;
+            } else {
+                if (sum != 0) {
+                    ans.push_back(sum);
+                    sum = 0;
+                }
+            }
+            x = x->next;
+        }
+
+       
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+
+        for (int val : ans) {
+            current->next = new ListNode(val);
+            current = current->next;
+        }
+
+        ListNode* result = dummy->next;
+        delete dummy;  
+        return result;
+    }
+};
