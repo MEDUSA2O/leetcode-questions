53638bfa8e9a309b8f6246cc10c798bc77e2237a
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-05 01:14:22 +0530

    Time: 61 ms (46.36%) | Memory: 36 MB (46.57%) - LeetSync

---
+++ 359-logger-rate-limiter/logger-rate-limiter.cpp
@@ -0,0 +1,25 @@
+class Logger {
+
+public:
+    unordered_map<string,int>mp;
+    const int TIMEOUT = 10;
+    Logger() {
+        
+        
+    }
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        if(mp.find(message) != mp.end() && mp[message] > timestamp){
+            return false;
+        }
+        mp[message] = timestamp +  TIMEOUT;
+        return true;
+        
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
