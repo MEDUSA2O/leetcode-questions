30fdaed1977a7f3c643ecb9a7ea5cd7e783d49c7
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-05 19:34:45 +0530

    Time: 31 ms (44.13%) | Memory: 15.7 MB (94.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] == '1') {
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                if(grid[i][j] == '1'){
                     count++;
-                    sink(grid, i, j);
+                    sink(grid , i , j );
                 }
             }
         }
         return count;
+        
     }
-    void sink(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() ||
-            grid[i][j] != '1') {
+    void sink(vector<vector<char>>& grid , int i , int j ){
+        if( i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] !='1' ){
             return;
         }
-        grid[i][j] = '0';
-        sink(grid, i + 1, j);
-        sink(grid, i - 1, j);
-        sink(grid, i, j - 1);
-        sink(grid, i, j + 1);
+        grid[i][j] = '2';
+        sink(grid , i + 1 , j );
+        sink(grid , i - 1 , j );
+        sink(grid , i , j + 1);
+        sink(grid , i , j - 1 );
+        
     }
+
 };
