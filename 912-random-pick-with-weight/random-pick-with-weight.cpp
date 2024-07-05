5efc184a1f017870cf3ab85e65322e790428f5f9
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-05 16:20:46 +0530

    Time: 57 ms (69.23%) | Memory: 44.1 MB (18.82%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.cpp
+++ 912-random-pick-with-weight/random-pick-with-weight.cpp
@@ -1,35 +1,33 @@
 class Solution {
 private:
-    vector<int> v; // Cumulative sum array
+    vector<int> v; 
 
 public:
     Solution(vector<int>& w) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-
-        // Initialize the cumulative sum array
         v.push_back(w[0]);
-        for (int i = 1; i < w.size(); ++i) {
-            v.push_back(v.back() + w[i]);
+        for( int i = 1 ; i < w.size() ; i++){
+            v.push_back(w[i] + v.back());
         }
+
     }
 
      int pickIndex() {
-        int rand_weight = rand() % v.back(); // Generate a random weight
-
-        // Manual binary search to find the first element greater than rand_weight
-        int low = 0, high = v.size() - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (v[mid] > rand_weight) {
-                high = mid; // We are still in the correct range, but not confirmed yet
-            } else {
-                low = mid + 1; // Move the low up to mid + 1 because mid is definitely too low
+        int random_no = rand() % v.back();
+        int l = 0 ;
+        int h = v.size() - 1;
+        while( l < h ){
+            int mid = l + ( h - l )/2;
+            if( v[mid] > random_no) h = mid;
+            else{
+                l = mid + 1;
             }
+
         }
+        return l;
         
-        return low; 
     }
 };
 
