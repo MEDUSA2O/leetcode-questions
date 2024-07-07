47d7e6ec6143e22f21e51afedbf2ef1bb46f074e
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-07 15:16:33 +0530

    Time: 1923 ms (12.70%) | Memory: 510.3 MB (5.15%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -0,0 +1,26 @@
+bool comparesoln(vector<int>a , vector<int>b){
+    return a[1] < b[1];
+}
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+       sort(intervals.begin(),intervals.end(),comparesoln);
+       int n = intervals.size();
+       int k = INT_MIN;
+       int ans =0 ;
+       for( int i = 0 ; i < n ; i++ ){
+        int x = intervals[i][0];
+        int y = intervals[i][1];
+        if( k <= x ){
+            k = y;
+        }
+        else{
+            ans++;
+        }
+        
+
+       }
+       return ans;
+
+    }
+};
