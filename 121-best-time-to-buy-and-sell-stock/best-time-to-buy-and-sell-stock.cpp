f6b6c486b200de006f71fdad89f2699906008bca
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-08 01:09:34 +0530

    Time: 60 ms (98.33%) | Memory: 96.2 MB (6.71%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
         int maxprofit = 0;
         int minprice = prices[0];
         int n = prices.size();
