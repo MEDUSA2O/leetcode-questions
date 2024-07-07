dc66351712124d45ebe646e754b69e884c490c3e
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-08 01:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (61.15%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,19 +3,17 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for (auto i : s) {
-            if (i == '(' || i == '[' || i == '{') {
+            if (i == '{' || i == '(' || i == '[') {
                 st.push(i);
             } else {
-                if (st.empty() || (st.top() == '(' && i != ')') ||
-                    (st.top() == '[' && i != ']') ||
-                    (st.top() == '{' && i != '}')) {
+                if (st.empty() || (st.top() == '{' && i != '}') ||
+                    (st.top() == '(' && i != ')') ||
+                    (st.top() == '[' && i != ']')) {
                     return false;
                 }
                 st.pop();
             }
-
         }
         return st.empty();
-        
     }
 };
