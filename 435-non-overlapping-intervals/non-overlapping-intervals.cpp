381476894a0135266c099207a7e7e8246927ede3
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-07 15:17:01 +0530

    Time: 2036 ms (7.06%) | Memory: 509.5 MB (13.08%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,3 +1,9 @@
+const int ZERO = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
 bool comparesoln(vector<int> a, vector<int> b) { return a[1] < b[1]; }
 class Solution {
 public:
