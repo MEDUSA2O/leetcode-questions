7765b7712b389ad6e2c074c3f7837316d9d92ab2
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-08 20:21:12 +0530

    Time: 24 ms (22.70%) | Memory: 25.6 MB (29.89%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,30 +1,17 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-
         queue<int> q;
-        // Fill the queue with numbers from 1 to n
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i <= n; i++) {
             q.push(i);
         }
-
-        // Simulate the process
         while (q.size() > 1) {
-            // Move the first k-1 people to the end of the queue
             for (int i = 0; i < k - 1; ++i) {
                 q.push(q.front());
                 q.pop();
             }
-            // Remove the k-th person
             q.pop();
         }
-
-        // The last remaining person is the winner
         return q.front();
     }
 };
