13ddffc4e8032da373895930a8f470cb502722c8
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-13 21:25:33 +0530

    Time: 295 ms (35.11%) | Memory: 229 MB (32.82%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions,
+                                      vector<int>& healths, string directions) {
+        unordered_map<int, int> index_map;
+        for (int i = 0; i < positions.size(); ++i) {
+            index_map[positions[i]] = i;
+        }
+
+        vector<int> stack; // Stack to keep track of robot indices
+
+        sort(positions.begin(), positions.end()); // Sort positions
+
+        for (int p : positions) {
+            int i = index_map[p];
+            if (directions[i] == 'R') {
+                stack.push_back(i); // Push index of robot moving to the right
+            } else {
+                // Handle collisions with robots moving to the right
+                while (!stack.empty() && directions[stack.back()] == 'R' &&
+                       healths[i] > 0) {
+                    int i2 = stack.back();
+                    stack.pop_back();
+                    if (healths[i] > healths[i2]) {
+                        healths[i2] = 0;
+                        healths[i] -= 1;
+                    } else if (healths[i] < healths[i2]) {
+                        healths[i] = 0;
+                        healths[i2] -= 1;
+                        stack.push_back(i2);
+                    } else {
+                        healths[i] = healths[i2] = 0;
+                    }
+                }
+                if (healths[i] > 0) {
+                    stack.push_back(i);
+                }
+            }
+        }
+
+        // Collect the healths of survived robots
+        vector<int> result;
+        for (int h : healths) {
+            if (h > 0) {
+                result.push_back(h);
+            }
+        }
+        return result;
+    }
+};
