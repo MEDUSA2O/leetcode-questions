74d85688fe606657419b9f5b4c334afceba52216
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-14 10:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.10%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        unordered_map<char, vector<int>> mp;
+        int s1 = s.size();
+        int s2 = t.size();
+
+        for (int i = 0; i < s2; i++) {
+            mp[t[i]].push_back(i);
+        }
+
+        int prev = -1;
+
+        for (int j = 0; j < s1; j++) {
+            char ch = s[j];
+
+            if (mp.find(ch) == mp.end()) {
+                return false;
+            }
+
+            vector<int> indices = mp[ch];
+            auto it = upper_bound(indices.begin(), indices.end(), prev);
+
+            if (it == indices.end()) {
+                return false;
+            }
+
+            prev = *it;
+        }
+
+        return true;
+    }
+}
+
+;
