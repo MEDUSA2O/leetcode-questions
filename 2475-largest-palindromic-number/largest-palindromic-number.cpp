d91fe3206b51d00c6e6c412a8a2edef1768e3297
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-18 14:01:35 +0530

    Time: 22 ms (76.06%) | Memory: 14.7 MB (88.12%) - LeetSync

--- 2475-largest-palindromic-number/largest-palindromic-number.cpp
+++ 2475-largest-palindromic-number/largest-palindromic-number.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    string largestPalindromic(string s) {
-        vector<int> v(10);
-        for(char c:s)v[c-'0']++;
+    string largestPalindromic(string num) {
+        vector<int>v(10);
+        for(char c : num)v[c - '0']++;
+        int single = -1;
         string ans;
-        int single=-1;
-        for(int i=9;i>=0;i--){
-            if(ans.empty() && i==0) continue;
-            while(v[i]>1){
-                ans.push_back(i+'0');
-                v[i]-=2;
+        for(int i = 9 ; i >= 0 ; i--){
+            if(ans.empty() && i == 0)continue;
+           while(v[i] > 1){
+                ans.push_back(i + '0');
+                v[i] -= 2;
             }
-            if(v[i]==1 && single==-1)single=i;
+            if(v[i] == 1 && single == -1 )single = i;
         }
-        string res=string(ans.rbegin(),ans.rend());
-        if(ans.empty() && single==-1) return "0";
-        if(single!=-1)
-            ans.push_back(single+'0');
-        ans+=res;
+        if(single == -1 && ans.empty() ) return "0";
+        string res = string ( ans.rbegin() , ans.rend());
+        if(single != -1){
+            ans.push_back(single + '0');
+        }
+        ans += res;
         return ans;
+
+        
     }
 };
