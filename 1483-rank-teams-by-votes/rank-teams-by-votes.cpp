64924fb1b5719d8884dfa03cc3e089ad2dd504c7
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-20 17:00:27 +0530

    Time: 11 ms (72.82%) | Memory: 14.9 MB (21.60%) - LeetSync

---
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        int n = votes.size();
+        int m = votes[0].size();
+        vector<vector<int>>vec(m,vector<int>(26,0));
+        for(auto j : votes){
+            for(int i = 0 ; i < m ; i++){
+                vec[i][j[i] - 'A']++;
+
+            }
+        }
+                string str = votes[0];
+
+        sort(str.begin() , str.end() , [&](char a , char b){
+            for(int i = 0 ; i < m ; i++){
+                if(vec[i][a - 'A'] == vec[i][b - 'A'])continue;
+                else return vec[i][a-'A'] > vec[i][b-'A'];
+
+            }
+            return a < b;
+
+        });
+        return str;
+    }
+};
