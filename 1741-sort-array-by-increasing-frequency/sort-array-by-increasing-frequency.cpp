a8ba3e33ec31ad6a61532867a32c7bd221855319
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-23 12:06:04 +0530

    Time: 4 ms (73.10%) | Memory: 15.2 MB (14.29%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -6,22 +6,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        // Step 1: Count the frequency of each element
-        unordered_map<int, int> freqMap;
+        unordered_map<int, int> freqmap;
         for (int num : nums) {
-            freqMap[num]++;
+            freqmap[num]++;
         }
-
-        // Step 2: Sort the nums vector with a custom comparator
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            // If frequencies are different, sort by frequency
-            if (freqMap[a] != freqMap[b]) {
-                return freqMap[a] < freqMap[b];
+        sort(nums.begin() , nums.end() , [&](int a , int b){
+            if(freqmap[a] != freqmap[b]){
+                return freqmap[a]<freqmap[b];
             }
-            // If frequencies are the same, sort by value descending
             return a > b;
         });
-
         return nums;
+
+       
+       
     }
 };
