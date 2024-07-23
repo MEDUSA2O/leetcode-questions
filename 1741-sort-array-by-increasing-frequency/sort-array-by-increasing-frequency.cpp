1c07452658960165a2dc60a7603eb11a73e91ac1
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-23 11:43:15 +0530

    Time: 3 ms (89.91%) | Memory: 14.9 MB (37.05%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -11,14 +11,11 @@ public:
         for (auto num : nums) {
             freqMap[num]++;
         }
-
-        // Step 2: Create a vector of pairs (element, frequency)
         vector<pair<int, int>> freqVec;
         for (auto pair : freqMap) {
             freqVec.push_back(pair);
         }
 
-        // Step 3: Sort the vector by frequency, and by value descending for same frequency
         sort(freqVec.begin(), freqVec.end(), [](pair<int, int>& a, pair<int, int>& b) {
             if (a.second == b.second) {
                 return a.first > b.first;
@@ -26,7 +23,6 @@ public:
             return a.second < b.second;
         });
 
-        // Step 4: Reconstruct the sorted array based on the sorted frequency
         vector<int> result;
         for (auto& pair : freqVec) {
             result.insert(result.end(), pair.second, pair.first);
