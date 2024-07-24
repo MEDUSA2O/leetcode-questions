24e762c46e238f3ce53799df13dfdffd9c75d172
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-24 14:29:19 +0530

    Time: 14 ms (70.75%) | Memory: 10.3 MB (66.53%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> c(26, 0); // Initialize vector for frequency counts
-
-        // Count frequencies of characters in magazine
-        for (auto j : magazine) {
-            c[j - 'a']++;
+        vector<int>c(26,0);
+        for(auto i : magazine){
+            c[i - 'a']++;
         }
-
-        // Check if ransom note can be constructed from the magazine
-        for (auto i : ransomNote) {
-            if (--c[i - 'a'] < 0) {
-                return false; // If count goes below zero, return false
+        for(auto j : ransomNote){
+            if(--c[j - 'a'] < 0){
+                return false;
             }
         }
-
-        return true; // If all characters in ransom note are found in magazine
+        return true;
+        
     }
 };
+
