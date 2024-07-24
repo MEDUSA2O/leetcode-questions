165ec49585a11ceb6e11882dce2faa6ffac00382
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-24 14:09:18 +0530

    Time: 30 ms (6.12%) | Memory: 11.8 MB (8.43%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char, int> mp1;
+        map<char,int>mp2;
+        for (auto i : magazine) {
+            mp1[i]++;
+        }
+        for (auto i : ransomNote) {
+            mp2[i]++;
+        }
+        for(auto i : mp2){
+            if(mp1.find(i.first) ==mp1.end()){
+                return false;
+            }
+            else{
+                if(mp1[i.first] < i.second ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
