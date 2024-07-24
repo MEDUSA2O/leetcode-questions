2902136f0dc8e2eb21f7dc0a45079c16c3d474c6
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-24 14:09:43 +0530

    Time: 23 ms (19.29%) | Memory: 11.8 MB (8.43%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+ Solution(){
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    }
     bool canConstruct(string ransomNote, string magazine) {
         map<char, int> mp1;
         map<char,int>mp2;
