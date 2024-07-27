19b499eeaed5e33e79e15b6b98c3879c17c9cbf6
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-27 11:08:29 +0530

    Time: 20 ms (78.81%) | Memory: 30.2 MB (5.83%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,6 +12,26 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        return root != NULL ? 1+countNodes(root->right)+countNodes(root->left) : 0;
+        // return root != NULL ? 1+countNodes(root->right)+countNodes(root->left) : 0;
+        if( root == NULL )return 0;
+        queue<TreeNode*>q;
+        int res = 0;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                TreeNode* t = q.front();
+                res++;
+                q.pop();
+                if( t-> left){
+                    q.push(t -> left);
+                }
+                 if( t-> right){
+                    q.push(t -> right);
+                }
+
+            }
+        }
+        return res;
     }
 };
