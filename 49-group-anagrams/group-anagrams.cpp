7496620a53fea6d3dcf1e0aced89009303541d3f
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-28 14:24:59 +0530

    Time: 35 ms (27.14%) | Memory: 25 MB (35.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mp;
-        for( auto i : strs){
-            string temp = i ;
+        for( auto i : strs ){
+            string temp = i;
             sort(temp.begin() , temp.end());
             mp[temp].push_back(i);
         }
-        vector<vector<string>>res;
-        for(auto m : mp){
-            res.push_back(m.second);
+        vector<vector<string>>ans;
+        for(auto m : mp ){
+            ans.push_back(m.second);
         }
-        return res;
+        return ans;
         
     }
 };
