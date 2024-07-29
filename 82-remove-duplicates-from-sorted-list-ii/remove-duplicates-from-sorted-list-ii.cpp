50d6e9b712551745019e395e0baceb2cd615d6c8
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-07-29 16:06:19 +0530

    Time: 10 ms (20.30%) | Memory: 16.9 MB (5.79%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -4,15 +4,15 @@ public:
         map<int, int> mp;
         ListNode* temp = head;
         ListNode* dummy = new ListNode(0);
-        ListNode* nh = dummy;  // Point nh to dummy initially
+        ListNode* nh = dummy;  
         
-        // Count occurrences of each value
+       
         while (head != NULL) {
             mp[head->val]++;
             head = head->next;
         }
 
-        // Create new list with non-duplicate values
+        
         for (auto i : mp) {
             if (i.second == 1) {
                 dummy->next = new ListNode(i.first);
@@ -20,6 +20,6 @@ public:
             }
         }
 
-        return nh->next;  // Return the next of dummy head, which points to the new head of the list
+        return nh->next;  
     }
 };
