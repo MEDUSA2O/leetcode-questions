967d19ff6a4571f8207121a9a3cd5038bbcf3ba2
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-01 10:54:45 +0530

    Time: 68 ms (90.41%) | Memory: 24.7 MB (36.08%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n = s.length();
-        stack<char>st;
+        stack<char> st;
         int ans = 0;
-        for(int i = 0 ; i < n ; i++ ){
-            if(!st.empty() && st.top() == 'b' && s[i] == 'a'){
+        for (int i = 0; i < n; i++) {
+            if (!st.empty() && st.top() == 'b' && s[i] == 'a') {
                 st.pop();
                 ans++;
 
-            }
-            else{
+            } else {
                 st.push(s[i]);
             }
         }
         return ans;
-
-        
     }
 };
