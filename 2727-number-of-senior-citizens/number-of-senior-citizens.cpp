3a8c0190ff9698fd9e3b7fa2f94263352ad13579
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-01 16:41:57 +0530

    Time: 4 ms (82.75%) | Memory: 17.5 MB (27.35%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int c = 0;
-        for (auto& i : details) {
-            // Convert the relevant substring to an integer and check if it's >= 60
+        for (auto i : details) {
             int age = stoi(i.substr(11, 2));
             if (age > 60) {
                 c++;
