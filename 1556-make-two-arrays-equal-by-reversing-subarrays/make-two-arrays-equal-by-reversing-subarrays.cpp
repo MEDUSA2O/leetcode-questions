12e7d9c980340ae501fd69dbded2a562a97b5b08
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-03 22:37:33 +0530

    Time: 12 ms (34.04%) | Memory: 21 MB (6.79%) - LeetSync

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        unordered_map<int, int> arrFreq;
+        for (int num : arr) {
+            arrFreq[num]++;
+        }
+
+        unordered_map<int, int> targetFreq;
+        for (int num : target) {
+            targetFreq[num]++;
+        }
+
+        if (arrFreq.size() != targetFreq.size()) {
+            return false;
+        }
+
+        for (auto it : arrFreq) {
+            if (targetFreq[it.first] != it.second) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
