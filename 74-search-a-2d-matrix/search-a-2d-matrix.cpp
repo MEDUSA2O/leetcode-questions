c20496e3f4ddebbae1ecd1bc7e4f904c59786e80
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-10 11:14:09 +0530

    Time: 7 ms (22.40%) | Memory: 12.2 MB (21.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    //     int a = matrix.size();
-    //     int b = matrix[0].size();
-    //     int start = 0;
-    //     int end = (a * b) - 1;
 
-    //     while (start <= end) {
-    //         int mid = (start + end) / 2;
-    //         int n = mid / b;
-    //         int m = mid % b;
-
-    //         if (matrix[n][m] == target)
-    //             return true;
-    //         else if (matrix[n][m] < target)
-    //             start = mid + 1;
-    //         else
-    //             end = mid - 1;
-    //     }
-    //     return false;
-    // }
-    int row = matrix.size() - 1;
-        int colm = 0 ;
-        while( row >= 0 && colm < matrix[0].size()){
-            if(matrix[row][colm] > target){
-                row --;
-            }
-            else if(matrix[row][colm] < target){
+        int row = matrix.size() - 1;
+        int colm = 0;
+        while (row >= 0 && colm < matrix[0].size()) {
+            if (matrix[row][colm] > target)
+                row--;
+            else if (matrix[row][colm] < target)
                 colm++;
-            }
-            else{
+            else
                 return true;
-            }
         }
         return false;
-        
     }
 };
