0bd67cb1c697b7536d31e74b133d3feb1a330b5e
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-17 16:51:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (38.69%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.cpp
+++ 408-valid-word-abbreviation/valid-word-abbreviation.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
-        int i = 0 , j = 0 ;
-        while( i < word.size() && j < abbr.size()){
-            if(isdigit(abbr[j])){
-                if(abbr[j] == '0'){
+        int i = 0, j = 0;
+        while (i < word.size() && j < abbr.size()) {
+            if (isdigit(abbr[j])) {
+                if (abbr[j] == '0') {
                     return false;
                 }
                 int num = 0;
-                while(isdigit(abbr[j]) && j <abbr.size()){
-                    num = num *10 + (abbr[j] - '0');
+                while (isdigit(abbr[j]) && j < abbr.size()) {
+                    num = num * 10 + (abbr[j] - '0');
                     j++;
                 }
-                i+= num;
-            }
-            else{
-                if(word[i] != abbr[j]){
+                i += num;
+            } else {
+                if (word[i] != abbr[j]) {
                     return false;
                 }
                 i++;
                 j++;
             }
         }
-return i == word.size() && j == abbr.size();        
+        return i == word.size() && j == abbr.size();
     }
 };
