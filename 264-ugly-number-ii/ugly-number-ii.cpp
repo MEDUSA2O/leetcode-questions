5114a9f0741131d24f557dd67a0b3d9b5b703afc
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-08-18 20:46:36 +0530

    Time: 6 ms (55.94%) | Memory: 9.7 MB (72.38%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> uglyNumbers(n);  // DP array to store ugly numbers
-        uglyNumbers[0] = 1;          // The first ugly number is 1
+        vector<int> uglyNumbers(n);  
+        uglyNumbers[0] = 1;          
 
-        // Three pointers for the multiples of 2, 3, and 5
         int indexMultipleOf2 = 0, indexMultipleOf3 = 0, indexMultipleOf5 = 0;
         int nextMultipleOf2 = 2, nextMultipleOf3 = 3, nextMultipleOf5 = 5;
 
-        // Generate ugly numbers until we reach the nth one
         for (int i = 1; i < n; i++) {
-            // Find the next ugly number as the minimum of the next multiples
             int nextUglyNumber =
                 min(nextMultipleOf2, min(nextMultipleOf3, nextMultipleOf5));
             uglyNumbers[i] = nextUglyNumber;
 
-            // Update the corresponding pointer and next multiple
             if (nextUglyNumber == nextMultipleOf2) {
                 indexMultipleOf2++;
                 nextMultipleOf2 = uglyNumbers[indexMultipleOf2] * 2;
@@ -30,6 +26,6 @@ public:
             }
         }
 
-        return uglyNumbers[n - 1];  // Return the nth ugly number
+        return uglyNumbers[n - 1];  
     }
 };
