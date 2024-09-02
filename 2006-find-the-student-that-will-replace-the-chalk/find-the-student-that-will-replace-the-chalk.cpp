3d0fdcc2d33e6219bb37c93369e33536fdd05a06
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-09-02 21:56:54 +0530

    Time: 103 ms (26.78%) | Memory: 77.1 MB (8.71%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long sum = 0;
+        for (auto i : chalk) {
+            sum += i;
+        }
+        int m = k % sum;
+        for (int i = 0 ; i < chalk.size() ; i++) {
+            if (m < chalk[i]) {
+                return i;
+            }
+            m -= chalk[i];
+        }
+        return -1;
+    }
+};
