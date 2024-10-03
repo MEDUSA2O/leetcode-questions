f06b8976c21658b1c76da2e1229c48860efdffb7
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-10-03 22:03:19 +0530

    Time: 3 ms (80.20%) | Memory: 11.2 MB (93.68%) - LeetSync

---
+++ 1098-largest-unique-number/largest-unique-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestUniqueNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            
+            if ((i == 0 || nums[i] != nums[i - 1]) && (i == nums.size() - 1 || nums[i] != nums[i + 1])) {
+                return nums[i]; 
+            }
+        }
+        return -1; 
+    }
+};
