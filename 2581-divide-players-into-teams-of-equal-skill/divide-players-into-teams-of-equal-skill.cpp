e12f23df8a90d80fc0186a4b3ecef26f1ff461bb
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-10-04 11:40:02 +0530

    Time: 64 ms (58.20%) | Memory: 56.4 MB (56.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,26 +1,37 @@
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         if(skill.size() == 2){
-            return skill[0]*skill[1];
+            return skill[0] * skill[1];
         }
+
         int n = skill.size();
         int tot = accumulate(skill.begin(), skill.end(), 0);
-        int target_energy = tot/(n/2);
+        int target_energy = tot / (n / 2);
         long long tc = 0;
-        sort(skill.begin(),skill.end());
-        for(int i = 0 ; i < n/2 ; i++){
-            int cs = skill[i] + skill[ n - i - 1];
-            if(cs != target_energy){
+        
+        sort(skill.begin(), skill.end());
+        
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int cs = skill[left] + skill[right];
+            if (cs != target_energy) {
                 return -1;
             }
-            tc += (long long)skill[i]*(long long)skill[n-i-1];
-
+            tc += (long long)skill[left] * (long long)skill[right];
+            left++;
+            right--;
         }
+
         return tc;
-    
-
-
-
     }
 };
