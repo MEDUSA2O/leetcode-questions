a34fe09434b5da5a0ef425090147e6c8d222af9b
MEDUSA <123243857+MEDUSA2O@users.noreply.github.com>
2024-12-07 02:36:02 +0530

    Time: 2382 ms (5.05%) | Memory: 117.7 MB (82.13%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -3,9 +3,8 @@ public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         int req = maxSum;
         int num = 0 ;
-        sort(banned.begin(), banned.end());
         for (int i = 1; i <= n; i++) {
-            auto result = std::find(banned.begin(), banned.end(), i);
+            auto result =find(banned.begin(), banned.end(), i);
             if(result == banned.end() && i <= req){
                 num++;
                 req -= i;
